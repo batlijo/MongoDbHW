65d76e0ccc88fd89605bb3982510b9f0c45a2227
Batli Joselevitz <batlijoselevitz@Batlis-MBP.attlocal.net>
2018-02-24 20:37:05 -0600

    it works?

--- public/app.js
+++ public/app.js
@@ -1,26 +1,25 @@
-// Grab the articles as a json
 $.getJSON("/articles", function(data) {
-  // For each one
+
   for (var i = 0; i < data.length; i++) {
-    // Display the apropos information on the page
+
     $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
   }
 });
 
 
-// Whenever someone clicks a p tag
+
 $(document).on("click", "p", function() {
-  // Empty the notes from the note section
+
   $("#notes").empty();
-  // Save the id from the p tag
+
   var thisId = $(this).attr("data-id");
 
-  // Now make an ajax call for the Article
+
   $.ajax({
     method: "GET",
     url: "/articles/" + thisId
   })
-    // With that done, add the note information to the page
+
     .then(function(data) {
       console.log(data);
       // The title of the article
